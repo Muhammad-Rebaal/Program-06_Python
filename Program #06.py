5dd2faa71ef96b3b62d877baef361986287dc12b
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-01-30 12:34:54 -0800

    Add files via upload

---
+++ Program #06.py
@@ -0,0 +1,9 @@
+# Program to give add the two digit numbers
+# just like if given 39 then 3+9 =12
+
+num= input("Enter Two digit Number! ")
+first_digit = float(num[0])
+second_digit = float(num[1])
+
+add = first_digit + second_digit
+print(add)
